41063ab318276d3ceee7c8c1b6c0317b14eb8196
tamilulagam <rajeshkumar.tfs@outlook.com>
2015-08-20 15:20:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'JWT Token Secret',
+  MONGO_URI: process.env.MONGO_URI || 'localhost',
+
+  // OAuth 2.0
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '',
+  FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || '',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || '',
+  GITHUB_SECRET: process.env.GITHUB_SECRET || '',
+  LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || '',
+  WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || '',
+  YAHOO_SECRET: process.env.YAHOO_SECRET || '',
+
+  // OAuth 1.0
+  TWITTER_KEY: process.env.TWITTER_KEY || '',
+  TWITTER_SECRET: process.env.TWITTER_SECRET || ''
+};
